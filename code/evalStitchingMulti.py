9e4731e2627dc25cdeae5fb60c10bcb7b69fb9fd
Matthew Fastow <matthewfastow@gmail.com>
2020-09-17 16:07:17 -0500

    evalstitchingmulti

--- code/evalStitchingMulti.py
+++ code/evalStitchingMulti.py
@@ -16,11 +16,42 @@ for fn in os.listdir(imagePath):
 # pair of images should be merged first (and of these, which one should be the "source" 
 # and which the "destination"), merge this pair, and proceed recursively.
 
-# YOUR CODE STARTS HERE
+keypoints = []
+descriptors = []
+for i in range(len(images)):
+	keypoints.append(detectKeypoints(images[i]))
+	descriptors.append(computeDescriptors(images[i], keypoints[i]))
+
+num_inliers_matrix = []
+for i in range(len(images)):
+	num_inliers_list = [0] * i
+	for j in range(i, len(images)):
+		matches = getMatches(descriptors[i], descriptors[j])
+		_, numInliers = RANSAC(matches, keypoints[i], keypoints[j])
+		num_inliers_list.append(numInliers)
+
+	num_inliers_matrix.append(num_inliers_list)
+
+
+for i in range(len(num_inliers_matrix)):
+	idx = np.argpartition(num_inliers_matrix[i], -2)[-2:]
+	indices = idx[np.argsort((-num_inliers_matrix[i])[idx])]
+
+	if num_inliers_matrix[i][indices[0]] / num_inliers_matrix[i][indices[1]]
+
+
+
+# compute num inliers between all pairs of images
+# start w *rightmost* image
+# determine by:
+# - getting num inliers between all images
+# - image w only one "friend" will be either leftmost or rightmost
+# - for these two images: determine if it's on "left" or "right" by 
+#   comparing *average col values* of its *keypoints that got matched*
 
 imCurrent = images[0]
 for im in images[1:]:
-    defaultH = np.array([[1,0,0], [0,1,0], [0,0,1]])
+    
     imCurrent = warpImageWithMapping(imCurrent, im, defaultH)
 
 cv2.imwrite(sys.argv[2], imCurrent)
